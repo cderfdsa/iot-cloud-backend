db4a90ee6a5c578701cee6364e0ab236695b51a4
weichuang <weichuang059@163.com>
2023-05-24 18:39:04 +0800

    1

--- server/server-mqtt/src/main/java/iot/cloud/backend/mqtt/MqttConnectAuth.java
+++ server/server-mqtt/src/main/java/iot/cloud/backend/mqtt/MqttConnectAuth.java
@@ -1,10 +1,10 @@
 package iot.cloud.backend.mqtt;
 
+import io.github.quickmsg.common.config.AuthConfig;
 import io.github.quickmsg.core.auth.IMqttConnectAuth;
 import iot.cloud.backend.service.modules.device.DeviceInfoService;
 import iot.cloud.backend.service.modules.user.UserInfoService;
 import iot.cloud.backend.service.utils.SpringApplicationUtils;
-import org.springframework.boot.SpringApplication;
 
 import java.nio.charset.StandardCharsets;
 
@@ -13,20 +13,23 @@ import java.nio.charset.StandardCharsets;
  */
 public class MqttConnectAuth implements IMqttConnectAuth {
     @Override
-    public boolean auth(String userName, byte[] passwordInBytes, String clientIdentifier1) {
+    public boolean auth(String userName, byte[] passwordInBytes, String clientIdentifier1, AuthConfig.SpiAuthConfig spiAuthConfig) {
         String password = new String(passwordInBytes, StandardCharsets.UTF_8);
-        
 
-        if ("default".equals(userName) && "Aa0123".equals(password)) {
-            return true;
-        }
-        SpringApplication springApplication = SpringApplicationUtils.getSpringApplication();
-        if (clientIdentifier1.startsWith("account:")) {
-            UserInfoService userInfoService = SpringApplicationUtils.getApplicationContext().getBean(UserInfoService.class);
-            return userInfoService.authForMqtt(userName, password);
-        } else {
+        if (clientIdentifier1.startsWith("device:")) {
             DeviceInfoService deviceInfoService = SpringApplicationUtils.getApplicationContext().getBean(DeviceInfoService.class);
             return deviceInfoService.auth(userName, password);
+        } else if (clientIdentifier1.startsWith("account:")) {
+            UserInfoService userInfoService = SpringApplicationUtils.getApplicationContext().getBean(UserInfoService.class);
+            return userInfoService.authForMqtt(userName, password);
+        } else if (clientIdentifier1.startsWith("manager:")) {
+            if (spiAuthConfig.getManagerUsername().equals(userName) && spiAuthConfig.getManagerPassword().equals(password)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
         }
     }
 }
