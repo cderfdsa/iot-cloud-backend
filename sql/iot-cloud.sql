b8e4c705283e75e5a509b5ab128db70a80e6393b
YiRanCN <cniiot@163.com>
2023-05-28 22:03:30 +0800

    1

--- sql/iot-cloud.sql
+++ sql/iot-cloud.sql
@@ -27,6 +27,9 @@ CREATE TABLE `device_type`
     `communication_type` int                                                          NOT NULL DEFAULT '1' COMMENT '通信方式 101WIFI 102以太网 103蜂窝网络2G 104蜂窝网络3G 105蜂窝网络4G 106蜂窝网络5G 107NB-IOT 199其他 201WIFI 202以太网 203蜂窝网络2G 204蜂窝网络3G 205蜂窝网络4G 206蜂窝网络5G 207NB-IOT 299其他 301WIFI 302以太网 303RS485 304RS232 305RS422 306LoRa 307Zigbee 308蓝牙/BLE 399其他',
     `protocol_type`      int                                                          NOT NULL DEFAULT '1' COMMENT '协议类型 1MQTT 2TCP 3HTTP',
     `protocol_format`    int                                                          NOT NULL DEFAULT '1' COMMENT '协议格式 101JSON 201ModbusRTU 202二进制 203普通文本 301JSON 302普通文本',
+    `rel_user_info_id`   bigint                                                       NOT NULL COMMENT '用户信息表',
+    `bus_time_value`     int                                                                   DEFAULT '10' COMMENT 'bus轮询时间 当protocol_type=2,protocol_format=201有效',
+    `bus_time_unit`      char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci              DEFAULT 's' COMMENT 'bus轮询时间单位 s秒 m分钟 h小时',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 8
@@ -67,6 +70,44 @@ CREATE TABLE `device_type_attribute_modbus`
   AUTO_INCREMENT = 9
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci COMMENT ='设备类型属性Modbus表';
+
+
+-- `iot-cloud`.history_device_attribute definition
+
+CREATE TABLE `history_device_attribute`
+(
+    `id`                         bigint      NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+    `rel_device_info_id`         bigint      NOT NULL COMMENT '设备信息表',
+    `rel_user_info_id`           bigint      NOT NULL COMMENT '用户信息表',
+    `device_name`                varchar(20) NOT NULL COMMENT '设备名称',
+    `device_code`                varchar(20) NOT NULL COMMENT '设备标识符',
+    `device_type_name`           varchar(20) NOT NULL COMMENT '设备类型名称',
+    `device_type_attribute_name` varchar(20) NOT NULL COMMENT '设备类型属性名称',
+    `device_type_attribute_code` varchar(20) NOT NULL COMMENT '设备类型属性标识符',
+    `create_dt`                  datetime    NOT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci COMMENT ='历史数据-设备属性';
+
+
+-- `iot-cloud`.history_device_online definition
+
+CREATE TABLE `history_device_online`
+(
+    `id`                 bigint      NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+    `rel_device_info_id` bigint      NOT NULL COMMENT '设备信息表',
+    `rel_user_info_id`   bigint      NOT NULL COMMENT '用户信息表',
+    `device_name`        varchar(20) NOT NULL COMMENT '设备名称',
+    `device_code`        varchar(20) NOT NULL COMMENT '设备标识符',
+    `create_dt`          datetime    NOT NULL COMMENT '创建时间',
+    `status`             int         NOT NULL COMMENT '1上线 2下线',
+    `status_reason`      int         NOT NULL COMMENT '1正常 21设备主动断开 22云端主动断开',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 7
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci COMMENT ='历史数据-设备上下线';
 
 
 -- `iot-cloud`.user_info definition
